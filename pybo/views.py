f12ac7da8af7643468ea41257b2cf2bb008000ea
whitegorae <whitegorae8@gmail.com>
2023-08-15 20:12:55 +0900

    질문 목록 조회 구현하기

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.")
+    """
+
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
